529ebb8e46751bdede88b06b5d3ddd911a22e278
Andy Zhao <andyzhao@google.com>
2022-05-06 12:32:18 -0700

    Add signer client. (#9)
    * Add initial CODEOWNERS
    
    * Add signer client.
    
    * Fix import path for client.go.
    
    * Improve config file support and update comments.
    
    * Update default config file logic.

---
+++ client/util/util_test.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestLoadSignerBinaryPath(t *testing.T) {
+	path, err := LoadSignerBinaryPath("./test_data/enterprise_certificate_config.json")
+	if err != nil {
+		t.Errorf("LoadSignerBinaryPath error: %q", err)
+	}
+	want := "C:/Program Files (x86)/Google/Endpoint Verification/signer.exe"
+	if path != want {
+		t.Errorf("Expected path is %q, got: %q", want, path)
+	}
+}
