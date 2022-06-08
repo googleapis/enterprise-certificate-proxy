b8aad1abda0827ac335b032bec2f3b0914a5c51c
Sijun Liu <liusijun1985@gmail.com>
2022-06-08 11:41:19 -0700

    update bin location

--- build/scripts/darwin_amd64.sh
+++ build/scripts/darwin_amd64.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create a folder to hold the binaries
-rm -rf ./build/darwin_amd64
-mkdir ./build/darwin_amd64
+rm -rf ./build/bin/darwin_amd64
+mkdir -p ./build/bin/darwin_amd64
 
 # Build the signer binary
 cd ./internal/signer/darwin
 go build
-mv signer ./../../../build/darwin_amd64
+mv signer ./../../../build/bin/darwin_amd64
 cd ./../../..
 
 # Build the signer library
-go build -buildmode=c-shared -o build/darwin_amd64/signer.dylib cshared/main.go
-rm build/darwin_amd64/signer.h
+go build -buildmode=c-shared -o build/bin/darwin_amd64/signer.dylib cshared/main.go
+rm build/bin/darwin_amd64/signer.h
--- build/scripts/linux_amd64.sh
+++ build/scripts/linux_amd64.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create a folder to hold the binaries
-rm -rf ./build/linux_amd64
-mkdir ./build/linux_amd64
+rm -rf ./build/bin/linux_amd64
+mkdir -p ./build/bin/linux_amd64
 
 # Build the signer library
-go build -buildmode=c-shared -o build/linux_amd64/signer.so cshared/main.go
-rm build/linux_amd64/signer.h
+go build -buildmode=c-shared -o build/bin/linux_amd64/signer.so cshared/main.go
+rm build/bin/linux_amd64/signer.h
 
 # (TODO) Build the signer binary
