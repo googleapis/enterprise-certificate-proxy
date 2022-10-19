d9ca76d46f002aa680791246cffe641ad39d2fe0
Andrew Tsay <tsaya@google.com>
2022-10-18 18:51:29 -0700

    feat: Add ARM64 build script

--- build/scripts/darwin_arm64.sh
+++ build/scripts/darwin_arm64.sh
@@ -3,15 +3,15 @@
 set -eu
 
 # Create a folder to hold the binaries
-rm -rf ./build/bin/darwin_amd64
-mkdir -p ./build/bin/darwin_amd64
+rm -rf ./build/bin/darwin_arm64
+mkdir -p ./build/bin/darwin_arm64
 
 # Build the signer binary
 cd ./internal/signer/darwin
 CGO_ENABLED=1 GO111MODULE=on GOARCH=arm64 go build
-mv signer ./../../../build/bin/darwin_amd64/ecp
+mv signer ./../../../build/bin/darwin_arm64/ecp
 cd ./../../..
 
 # Build the signer library
-CGO_ENABLED=1 GO111MODULE=on GOARCH=arm64 go build -buildmode=c-shared -o build/bin/darwin_amd64/libecp.dylib cshared/main.go
-rm build/bin/darwin_amd64/libecp.h
+CGO_ENABLED=1 GO111MODULE=on GOARCH=arm64 go build -buildmode=c-shared -o build/bin/darwin_arm64/libecp.dylib cshared/main.go
+rm build/bin/darwin_arm64/libecp.h
