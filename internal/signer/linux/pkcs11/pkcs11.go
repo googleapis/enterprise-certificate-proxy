ff40aa559eeb4917f692061bb8e796a01b383636
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2023-06-15 09:53:57 -0700

    fix: Add bounds check to PKCS #11 object arrays. (#82)
    * fix: Add bounds check to PKCS #11 object arrays. Mislabeled objects cause a segfault otherwise.

--- internal/signer/linux/pkcs11/pkcs11.go
+++ internal/signer/linux/pkcs11/pkcs11.go
@@ -18,6 +18,7 @@ package pkcs11
 import (
 	"crypto"
 	"errors"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
@@ -55,6 +56,11 @@ func Cred(pkcs11Module string, slotUint32Str string, label string, userPin strin
 	if err != nil {
 		return nil, err
 	}
+
+	if len(certs) < 1 {
+		return nil, fmt.Errorf("No certificate object was found with label %s.", label)
+	}
+
 	cert, err := certs[0].Certificate()
 	if err != nil {
 		return nil, err
@@ -70,6 +76,11 @@ func Cred(pkcs11Module string, slotUint32Str string, label string, userPin strin
 	if err != nil {
 		return nil, err
 	}
+
+	if len(pubKeys) < 1 {
+		return nil, fmt.Errorf("No public key object was found with label %s.", label)
+	}
+
 	pubKey, err := pubKeys[0].PublicKey()
 	if err != nil {
 		return nil, err
@@ -79,6 +90,11 @@ func Cred(pkcs11Module string, slotUint32Str string, label string, userPin strin
 	if err != nil {
 		return nil, err
 	}
+
+	if len(privkeys) < 1 {
+		return nil, fmt.Errorf("No private key object was found with label %s.", label)
+	}
+
 	privKey, err := privkeys[0].PrivateKey(pubKey)
 	if err != nil {
 		return nil, err
