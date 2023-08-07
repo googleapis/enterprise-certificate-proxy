ab0ee9dc129c5c02ad82e18acf9e1eac142cc4c7
Angela Hao <angelahao@google.com>
2023-08-07 16:33:18 -0700

    Fixed script string

--- cred/scripts/keychain_cred.sh
+++ cred/scripts/keychain_cred.sh
@@ -20,7 +20,7 @@ WORK_DIR=$(mktemp -d)
 
 pushd ${WORK_DIR}
 
-openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 5 -nodes -subj "/C=US/ST=WA/L=Kirkland/O=Temp/OU=CI/CN=Test>
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 5 -nodes -subj "/C=US/ST=WA/L=Kirkland/O=Temp/OU=CI/CN=TestIssuer/emailAddress=dev@example.com"
 openssl pkcs12 -inkey key.pem -in cert.pem -export -out cred.p12 -passin pass:${PASSWORD} -passout pass:${PASSWORD}
 security import cred.p12 -P ${PASSWORD} -A
 
