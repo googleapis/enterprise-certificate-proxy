bc2872769fec66b61d4a06c1bf36de305de98a62
Nolan Eastin <neastin@google.com>
2025-10-07 17:28:01 +0000

    ECP Proxy: Add file-level comments, rename mtls_test

--- http_proxy/test_passthrough_proxy_test.go
+++ http_proxy/test_passthrough_proxy_test.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This file contains tests for the test passthrough proxy.
 package main
 
 import (
