bfaa3e886b666d7c58c6723f7f08a31c07081460
agrawalradhika-cell <agrawalradhika@google.com>
2026-07-29 14:05:57 -0700

    Refactor logging to use custom logger for linux signer
    Replaced log package with custom logger and removed ECP logging function.

--- internal/signer/linux/signer.go
+++ internal/signer/linux/signer.go
@@ -24,25 +24,14 @@ import (
 	"crypto/x509"
 	"encoding/gob"
 	"io"
-	"log"
 	"net/rpc"
 	"os"
 	"time"
 
+	"github.com/googleapis/enterprise-certificate-proxy/internal/logger"
 	"github.com/googleapis/enterprise-certificate-proxy/internal/signer/linux/pkcs11"
 	"github.com/googleapis/enterprise-certificate-proxy/internal/signer/util"
 )
-
-// If ECP Logging is enabled return true
-// Otherwise return false
-func enableECPLogging() bool {
-	if os.Getenv("ENABLE_ENTERPRISE_CERTIFICATE_LOGS") != "" {
-		return true
-	}
-
-	log.SetOutput(io.Discard)
-	return false
-}
 
 func init() {
 	gob.Register(crypto.SHA256)
@@ -123,24 +112,23 @@ func (k *EnterpriseCertSigner) Decrypt(args DecryptArgs, resp *[]byte) (err erro
 }
 
 func main() {
-	enableECPLogging()
 	if len(os.Args) != 2 {
-		log.Fatalln("Signer is not meant to be invoked manually, exiting...")
+		logger.Fatal("Signer is not meant to be invoked manually, exiting...")
 	}
 	configFilePath := os.Args[1]
 	config, err := util.LoadConfig(configFilePath)
 	if err != nil {
-		log.Fatalf("Failed to load enterprise cert config: %v", err)
+		logger.Fatalf("Failed to load enterprise cert config: %v", err)
 	}
 
 	enterpriseCertSigner := new(EnterpriseCertSigner)
 	enterpriseCertSigner.key, err = pkcs11.Cred(config.CertConfigs.PKCS11.PKCS11Module, config.CertConfigs.PKCS11.Slot, config.CertConfigs.PKCS11.Label, config.CertConfigs.PKCS11.UserPin)
 	if err != nil {
-		log.Fatalf("Failed to initialize enterprise cert signer using pkcs11: %v", err)
+		logger.Fatalf("Failed to initialize enterprise cert signer using pkcs11: %v", err)
 	}
 
 	if err := rpc.Register(enterpriseCertSigner); err != nil {
-		log.Fatalf("Failed to register enterprise cert signer with net/rpc: %v", err)
+		logger.Fatalf("Failed to register enterprise cert signer with net/rpc: %v", err)
 	}
 
 	// If the parent process dies, we should exit.
@@ -149,7 +137,7 @@ func main() {
 	go func() {
 		for {
 			if os.Getppid() == 1 {
-				log.Fatalln("Enterprise cert signer's parent process died, exiting...")
+				logger.Fatal("Enterprise cert signer's parent process died, exiting...")
 			}
 			time.Sleep(time.Second)
 		}
